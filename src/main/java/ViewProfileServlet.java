039e7026d1fc316206367dab92a9d557814ada3d
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-21 16:59:16 -0500

    sept 21

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -8,6 +8,12 @@ import java.io.IOException;
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+        } else {
+            String user = (String) request.getSession().getAttribute("user");
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        }
     }
 }
---
+++ src/main/webapp/WEB-INF/register.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Log Out</title>
+</head>
+<body>
+<h1>Logging out</h1>
+</body>
+</html>
